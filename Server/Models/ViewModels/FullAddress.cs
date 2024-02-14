2279064a937c25b56557b1707887bb21eb9a7e4c
Hanzhi <hanzhizhang11@gmail.com>
2024-02-14 13:16:17 +1000

    added full address view model

---
+++ Server/Models/ViewModels/FullAddress.cs
@@ -0,0 +1,11 @@
+namespace Server.ViewModels;
+
+public class FullAddress
+{
+    public string UnitNumber {get; set;} = "";
+    public string StreetNumber {get; set;} = "";
+    public string Street {get; set;} = "";
+    public string Suburb {get; set;} = "";
+    public string PostCode {get; set;} = "";
+    public string State {get;set;} = "";
+}
