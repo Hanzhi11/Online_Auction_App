817cfbe627ab20f17e5cbf6a115cd5bca31b0a68
Hanzhi <hanzhizhang11@gmail.com>
2024-02-14 13:15:00 +1000

    added address controller

---
+++ Server/Controllers/AddressController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Server.Data;
+using Server.Models;
+using Server.ViewModels;
+
+namespace Server.Controllers;
+
+public class AddressController(AppDbContext context) : ControllerBase
+{
+    private readonly AppDbContext _context = context;
+
+    public IActionResult Index()
+    {
+        List<Address> addresses = [.. _context.Address];
+        List<FullAddress> fullAddresses = [];
+        foreach (Address address in addresses)
+        {
+            State state = _context.State.FirstOrDefault(s => s.PostCode == address.StatePostCode)!;
+            string stateName = state.Name;
+
+            FullAddress fulladdress = new()
+            {
+                UnitNumber = address.UnitNumber,
+                StreetNumber = address.StreetNumber,
+                Street = address.Street,
+                Suburb = address.Suburb,
+                PostCode = address.StatePostCode,
+                State = stateName
+            };
+            fullAddresses.Add(fulladdress);
+        }
+
+        return Ok(fullAddresses);
+    }
+}
