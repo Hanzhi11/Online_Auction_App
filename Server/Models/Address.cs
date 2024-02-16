9a5ada90494e8c99ff8572181376bed1a3580270
Hanzhi <hanzhizhang11@gmail.com>
2024-02-16 11:54:11 +1000

    added formattofulladress method to address

--- Server/Models/Address.cs
+++ Server/Models/Address.cs
@@ -1,19 +1,35 @@
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Diagnostics.CodeAnalysis;
+using Server.ViewModels;
 
 namespace Server.Models;
 
-[method: SetsRequiredMembers]
 public class Address(string streetNumber, string street, string suburb, string statePostCode)
 {
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public Guid Id { get; set; }
 
-    public string UnitNumber {get; set;} = "";
-    public string StreetNumber {get; set;} = streetNumber;
-    public string Street {get; set;} = street;
-    public string Suburb {get; set;} = suburb;
-    public string StatePostCode {get; set;} = statePostCode;
+    public string UnitNumber { get; set; } = "";
+    public string StreetNumber { get; set; } = streetNumber;
+    public string Street { get; set; } = street;
+    public string Suburb { get; set; } = suburb;
+    public string StatePostCode { get; set; } = statePostCode;
 
-    public State? State { get; set;}
+    public State? State { get; set; }
+
+    public FullAddressViewModel FormatToFullAddress()
+    {
+        FullAddressViewModel fullAddress = new();
+        
+        if (State != null)
+        {
+            fullAddress.UnitNumber = UnitNumber;
+            fullAddress.StreetNumber = StreetNumber;
+            fullAddress.Street = Street;
+            fullAddress.Suburb = Suburb;
+            fullAddress.PostCode = StatePostCode;
+            fullAddress.State = State.Name;
+        }
+
+        return fullAddress;
+    }
 }
