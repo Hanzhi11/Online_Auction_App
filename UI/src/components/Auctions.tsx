250aba10dc75392ab24563056a48734d7e1a68b2
Hanzhi <hanzhizhang11@gmail.com>
2024-02-23 21:20:22 +1000

    add more properties to interfaces

--- UI/src/components/Auctions.tsx
+++ UI/src/components/Auctions.tsx
@@ -3,8 +3,9 @@ import SectionContainer from "./SectionContainer";
 import { IconContext } from "react-icons";
 import { FaPlus } from "react-icons/fa6";
 import { STATE } from "./Constants";
+import { Link } from 'react-router-dom'
 
-interface FullAddress {
+export interface FullAddress {
     unitNumber: string;
     streetNumber: string;
     street: string;
@@ -18,6 +19,7 @@ interface ListingInfo {
     auctionDateTime: Date;
     agencyName: string;
     photoDataURL: string;
+    listingNumber: number;
 }
 
 function Auctions() {
@@ -143,6 +145,7 @@ function Auctions() {
     const content = listingsInfo.map((listingInfo, index) => {
         const address = listingInfo.address
         let fullAddress = address.streetNumber + ' ' + address.street + ', ' + address.suburb
+        const routeFrag = address.suburb.replace(' ', '+') + '-' + address.state + '-' + listingInfo.listingNumber
         if (address.unitNumber) {
             fullAddress = address.unitNumber + '/' + fullAddress
         }
@@ -167,7 +170,7 @@ function Auctions() {
         const anchorStyle = 'ml-auto hover:cursor-pointer border-b border-transparent hover:border-green-500 self-center'
 
         return (
-            <div className="transition-width duration-0 xl:duration-500 ease-in-out w-[315px] h-52 relative md:w-[345px] lg:w-[294px] xl:w-[360px] overflow-hidden group rounded-md text-white bg-cover" style={{ backgroundImage: `url('${listingInfo.photoDataURL}')` }} key={index}>
+            <div className="transition-width duration-0 xl:duration-500 ease-in-out w-[315px] h-52 relative md:w-[345px] lg:w-[294px] xl:w-[360px] overflow-hidden group rounded-md text-white bg-cover" style={{ backgroundImage: `url('${listingInfo.photoDataURL}')`}} key={index}>
                 <header className={'bg-indigo-900 bg-opacity-70 h-10 px-5 py-2.5 text-sm truncate ' + defaultOpacityTransition}>{fullAddress}</header>
                 <span className={"bg-white font-medium px-2 py-1 absolute bottom-2.5 right-2 text-black " + defaultOpacityTransition}>{listingInfo.agencyName}</span>
                 <div className={"absolute inset-0 grid grid-rows-6 grid-cols-2 bg-indigo-900 bg-opacity-70 text-sm font-medium h-full p-5 pb-12 " + overLayOpacityTransition}>
@@ -175,7 +178,7 @@ function Auctions() {
                     <p className="col-span-2">{formattedDate}</p>
                     <p className="text-green-500 col-span-2">{listingInfo.agencyName}</p>
                     <p>Auction Listing</p>
-                    <a className={anchorStyle}>VIEW</a>
+                    <Link className={anchorStyle} to={`/listing/${routeFrag}`}>VIEW</Link>
                     <p>Register to BID</p>
                     <a className={"text-green-500 " + anchorStyle}>OPEN</a>
                 </div>
@@ -187,7 +190,7 @@ function Auctions() {
         <IconContext.Provider value={{ color: 'white', size: '1.2rem', className: 'inline stroke-[20]' }}>
             <SectionContainer header='Upcoming Auctions'>
                 <div className="grid grid-cols-1 justify-items-center gap-y-4 md:gap-x-6 md:grid-cols-2 lg:grid-cols-3">{content}</div>
-                <button className="block mx-auto mt-5 bg-green-500 rounded-full w-10 h-10" onClick={() => { }}><FaPlus /></button>
+                <button className="block mx-auto my-5 bg-green-500 rounded-full w-10 h-10" onClick={() => { }}><FaPlus /></button>
             </SectionContainer>
         </IconContext.Provider >
     )
