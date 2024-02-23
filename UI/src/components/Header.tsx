07234282dac97e8c925d1908df8db9a42a110744
Hanzhi <hanzhizhang11@gmail.com>
2024-02-23 21:19:16 +1000

    refactored header and home component

--- UI/src/components/Header.tsx
+++ UI/src/components/Header.tsx
@@ -1,108 +1,148 @@
 import { IconContext } from "react-icons";
 import { GiHamburgerMenu } from "react-icons/gi";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import OverLay from "./OverLay";
-import className from 'classnames';
+import className from "classnames";
 import NavBar from "./NavBar";
-import { ELEMENT_ID } from './Constants'
+import { ELEMENT_ID } from "./Constants";
 import Logo from "./Logo";
 import SectionContainer from "./SectionContainer";
+import { useLocation } from "react-router-dom";
 
 function Header() {
+    const location = useLocation();
+    const isHomePage = location.pathname === "/";
+
     const [overLayStatus, setOverLayStatus] = useState({
         isOpen: false,
-        isForNav: true
-    })
-    const [offset, setOffset] = useState(0)
+        isForNav: true,
+    });
+    const [offset, setOffset] = useState(0);
     const [windowSize, setWindowSize] = useState({
         width: window.innerWidth,
-        height: window.innerHeight
-    })
+        height: window.innerHeight,
+    });
+    const [isTransparent, setIsTransparent] = useState(false);
 
-    window.onscroll = () => {
-        setOffset(window.scrollY)
-    }
+    useEffect(() => {
+        const homeRunner = document.getElementById(
+            ELEMENT_ID.HOME_RUNNER
+        ) as HTMLElement;
+        if (!window.onresize) {
+            window.onresize = () => {
+                setWindowSize({
+                    width: window.innerWidth,
+                    height: window.innerHeight,
+                });
+            };
+        }
 
-    window.onresize = () => {
-        setWindowSize({
-            width: window.innerWidth,
-            height: window.innerHeight
-        })
-    }
+        if (!homeRunner) {
+            setIsTransparent(false);
+            window.onscroll = null;
+            return;
+        }
+
+        if (!window.onscroll) {
+            window.onscroll = () => {
+                setOffset(window.scrollY);
+            };
+        }
+
+        let newIsTransparent = false;
+        const homeRunnerHeight = homeRunner.offsetHeight;
+        if (homeRunnerHeight - offset > 112) {
+            newIsTransparent = true;
+        }
+        setIsTransparent(newIsTransparent);
+    }, [offset, windowSize, location.pathname]);
 
     const handleOnClick = (event: React.MouseEvent<Element>) => {
-        let button = event.target as HTMLElement
-        if (button?.tagName !== 'BUTTON') {
-            button = button?.closest('button') as HTMLElement
+        let button = event.target as HTMLElement;
+        if (button?.tagName !== "BUTTON") {
+            button = button?.closest("button") as HTMLElement;
         }
         if (button) {
-            const id = button.id
+            const id = button.id;
             switch (id) {
                 case ELEMENT_ID.SIGN_IN_BUTTON:
                     setOverLayStatus({
                         isOpen: true,
-                        isForNav: false
-                    })
-                    break
+                        isForNav: false,
+                    });
+                    break;
                 case ELEMENT_ID.NAV_OPEN_BUTTON:
                     setOverLayStatus({
                         isOpen: true,
-                        isForNav: true
-                    })
-                    break
+                        isForNav: true,
+                    });
+                    break;
                 case ELEMENT_ID.CLOSE_OVERLAY_BUTTON:
                     setOverLayStatus({
                         isOpen: false,
-                        isForNav: true
-                    })
-                    break
+                        isForNav: true,
+                    });
+                    break;
             }
         }
-    }
+    };
 
-    const homeRunner = document.getElementById(ELEMENT_ID.HOME_RUNNER) as HTMLElement
-    const isTransparent = homeRunner?.offsetHeight !== 112 && offset <= homeRunner?.offsetHeight - 112
-    const isSmallScreen = windowSize.width < 768
+    const isSmallScreen = windowSize.width < 768;
 
-    const topStyle = className('bg-indigo-900 h-20 flex justify-center', {
-        'bg-opacity-70': isTransparent
-    })
+    const topStyle = className("bg-indigo-900 h-20 flex justify-center", {
+        "bg-opacity-70": isTransparent,
+    });
 
-    const bottomStyle = className('bg-green-500 h-8', {
-        'bg-opacity-90 saturate-75': isTransparent
-    })
+    const bottomStyle = className("bg-green-500 h-8", {
+        "bg-opacity-90 saturate-75": isTransparent,
+    });
 
-
-    let iconSize = '1rem'
+    let iconSize = "1rem";
     if (overLayStatus.isOpen || isSmallScreen) {
-        iconSize = '1.8rem'
+        iconSize = "1.8rem";
     }
 
-    const navContent = <NavBar isSmallScreen={overLayStatus.isOpen || isSmallScreen} onSignIn={handleOnClick} />
+    const navContent = (
+        <NavBar
+            isSmallScreen={overLayStatus.isOpen || isSmallScreen}
+            onSignIn={handleOnClick}
+        />
+    );
 
-    let content
+    let content;
     if (isSmallScreen) {
         content = (
-            <button id={ELEMENT_ID.NAV_OPEN_BUTTON} className="md:hidden hover:cursor-pointer my-auto" onClick={handleOnClick}>
-                < GiHamburgerMenu />
+            <button
+                id={ELEMENT_ID.NAV_OPEN_BUTTON}
+                className="md:hidden hover:cursor-pointer my-auto"
+                onClick={handleOnClick}
+            >
+                <GiHamburgerMenu />
             </button>
-        )
+        );
     } else {
-        content = navContent
+        content = navContent;
     }
 
     if (overLayStatus.isOpen) {
-        const overLayContent = overLayStatus.isForNav ? navContent : <div>sign in</div>
-        content = (
-            <OverLay onClose={handleOnClick}>
-                {overLayContent}
-            </OverLay>
-        )
+        const overLayContent = overLayStatus.isForNav ? (
+            navContent
+        ) : (
+            <div>sign in</div>
+        );
+        content = <OverLay onClose={handleOnClick}>{overLayContent}</OverLay>;
     }
 
+    const headerStyle = className("w-full top-0 z-9999", {
+        fixed: isHomePage,
+        sticky: !isHomePage,
+    });
+
     return (
-        <IconContext.Provider value={{ color: 'white', size: iconSize, className: 'inline' }}>
-            <header className="fixed top-0 w-full" id={ELEMENT_ID.HEADER}>
+        <IconContext.Provider
+            value={{ color: "white", size: iconSize, className: "inline" }}
+        >
+            <header className={headerStyle} id={ELEMENT_ID.HEADER}>
                 <div className={topStyle}>
                     <SectionContainer style="flex">
                         <Logo />
@@ -111,8 +151,16 @@ function Header() {
                 </div>
                 <div className={bottomStyle}></div>
             </header>
-        </IconContext.Provider >
-    )
+            {isHomePage && (
+                <div
+                    className="h-52 max-h-[25vh] min-h-[7rem] overflow-hidden"
+                    id={ELEMENT_ID.HOME_RUNNER}
+                >
+                    <img src="/runner.jpeg" />
+                </div>
+            )}
+        </IconContext.Provider>
+    );
 }
 
-export default Header
+export default Header;
--- UI/src/components/Home.tsx
+++ UI/src/components/Home.tsx
@@ -1,22 +1,12 @@
-import { ELEMENT_ID } from './Constants'
-import Header from './Header'
 import Search from './Search'
 import Auctions from './Auctions'
-import Footer from './Footer'
 
 function Home() {
   return (
-    <>
       <main>
-        <div className='h-52 max-h-[25vh] min-h-[7rem] overflow-hidden' id={ELEMENT_ID.HOME_RUNNER}>
-          <img src='/runner.jpeg' />
-        </div>
         <Search />
         <Auctions />
-        <Footer />
       </main>
-      <Header />
-    </>
   )
 }
 
